e40fb64b3ab439ae2a99b0580f45dfdcbe3e6d72
bhargavi <bhargavij361@example.com>
2022-11-23 12:48:12 +0530

    second

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let label=document.getElementById('in')
 let button=document.getElementById('any')
 let span=document.getElementById('inp')
+let div=document.getElementById('ma')
 const print = () => {
     if(label.value==100){
         console.log('we have chocolates on this price')
         span.innerHTML="we have chocolates on this price"
         // alert('we have chocolates on this price')
-        span.style.color="green"
+        span.style.color="white"
+        div.style.backgroundImage="url(https://images.unsplash.com/photo-1614338143359-470838a54a8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80)"
+        div.style.backgroundPosition="center"
+        div.style.backgroundRepeat="norepeat"
+        div.style.backgroundSize="cover"
+
+
 
     }
     else if(label.value==200){
         console.log('we have biscuits on this price')
         span.innerHTML="we have biscuits on this price"
         // alert('we have biscuits on this price')
-        span.style.color="green"
+        span.style.color="black"
+        div.style.backgroundImage="url(https://images.unsplash.com/photo-1551584277-a31a25e08fc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)"
+        div.style.backgroundPosition="center"
+        div.style.backgroundRepeat="norepeat"
+        div.style.backgroundSize="cover"
 
         
     }
@@ -21,7 +32,11 @@ const print = () => {
         console.log('we have bingo on this price')
         span.innerHTML="we have bingo on this price"
         // alert('we have bingo on this price')
-        span.style.color="green"
+        span.style.color="black"
+        div.style.backgroundImage="url(https://images.unsplash.com/photo-1612773843298-44dcdd45d865?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80)"
+        div.style.backgroundPosition="center"
+        div.style.backgroundRepeat="norepeat"
+        div.style.backgroundSize="cover"
 
 
         
@@ -31,6 +46,10 @@ const print = () => {
         span.innerHTML="we don't have anything on this price"
 
         span.style.color="red"
+        div.style.backgroundImage="url()"
+        div.style.backgroundPosition="center"
+        div.style.backgroundRepeat="norepeat"
+        div.style.backgroundSize="cover"
 
     }
 }
