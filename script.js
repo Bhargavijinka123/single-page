89f142b0d8e855d950511cda68736c08a1a160f1
bhargavi <bhargavij361@example.com>
2022-11-27 18:18:51 +0530

    first

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
- const show =()=>{
-console.log('helllo') 
-document.getElementById('hero').style.display="flex"
-
-}
 const clos =()=>{
-    // alert("world")
-document.getElementById('hero').style.display="none"
-    
+
+console.log(hello)
+document.getElementById(hero).style.display="flex"
 }
+const show =()=>{
+
+    document.getElementById(main1).style.display="none"
+    
+    }
