ca2c82bb194730bd9511711967261f69d26f218e
bhargavi <bhargavij361@example.com>
2022-12-05 11:42:22 +0530

    first

---
+++ bg/test/script.js
@@ -0,0 +1,15 @@
+ const show=()=>{
+    let in1=document.getElementById('one').value
+    let in2=document.getElementById('two').value
+    
+
+console.log(in1,in2)
+
+let r1= in1.toUpperCase();
+let r2= in2.toUpperCase();
+document.getElementById('ab').innerHTML=r1  
+
+document.getElementById('cd').innerHTML=r2
+
+
+ }
