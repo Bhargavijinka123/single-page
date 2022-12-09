dee9c976425a61822097397f2b0af6b046a786e7
bhargavi <bhargavij361@example.com>
2022-12-09 11:56:03 +0530

    six

--- test/script.js
+++ test/script.js
@@ -1,1 +1,17 @@
-//  
+
+const show =()=>{
+    document.getElementById('ca1').style.display="block"
+}
+
+const sh =()=>{
+    document.getElementById('ca2').style.display="block"
+}
+const sho =()=>{
+    document.getElementById('ca3').style.display="block"
+}
+
+const clos =()=>{
+    document.getElementById('ca1').style.display="none"
+    document.getElementById('ca2').style.display="none"
+    document.getElementById('ca3').style.display="none"
+}
