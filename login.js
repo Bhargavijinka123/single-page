4cc7a98a8b2fcd4834265971bad204e5e300c0c6
bhargavi <bhargavij361@example.com>
2022-12-09 13:15:46 +0530

    seven

---
+++ login.js
@@ -0,0 +1,15 @@
+
+const show=()=>{
+
+    let a=document.getElementById('ab').value
+    let b=document.getElementById('cd').value
+if(a=="bhargavi@gmail.com" && b=="bhargavi"){
+    // alert('success')
+    window.location.assign("/index.html")
+}
+else{
+    alert('failed')
+}
+
+}
+
