5b747fab57821513511a7ba9cdcd35b5184afc4f
bhargavi <bhargavij361@example.com>
2022-12-10 14:48:49 +0530

    ei

--- login.js
+++ login.js
@@ -1,10 +1,8 @@
 
 const show=()=>{
-
     let a=document.getElementById('ab').value
     let b=document.getElementById('cd').value
 if(a=="bhargavi@gmail.com" && b=="bhargavi"){
-    // alert('success')
     window.location.assign("/index.html")
 }
 else{
