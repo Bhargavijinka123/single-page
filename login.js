4b2352fc3bf9746bb5ec015cf04721c0ad8aa51c
bhargavi <bhargavij361@example.com>
2022-12-12 13:45:26 +0530

    first

--- login.js
+++ login.js
@@ -3,11 +3,15 @@ const show=()=>{
     let a=document.getElementById('ab').value
     let b=document.getElementById('cd').value
 if(a=="bhargavi@gmail.com" && b=="bhargavi"){
-    window.location.assign("/index.html")
+    window.location.assign("./index.html")
 }
 else{
-    alert('failed')
+    window.location.assign("./warn.html")
 }
+}
+const back =()=>{
+    document.getElementById('btn').style.display="none"
+    document.getElementById('main').style.display="flex"
+    window.location.assign("./login.html")
 
 }
-
